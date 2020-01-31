d49d3bf081ce5a3e9b7f922d8ca7d67bcc7a50a9
Carlos <Carlos@CARLOS>
2020-01-31 20:56:06 +0100

    Mejora9

--- src/main/java/Decimales.java
+++ src/main/java/Decimales.java
@@ -1,6 +1,13 @@
 package main.java;
 
 public class Decimales {
+	
+	public static void redondear (double numeroD) {
+			
+		System.out.print("El numero redondeado al alza es: ");
+		System.out.println((Math.round(numeroD)));
+			
+	}
 
 	public static void main(String[] args) {
 		double decimal=5.7;
