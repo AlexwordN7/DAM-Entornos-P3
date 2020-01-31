fb9d1b6e31483fba4cd3eb665e59ee97f671f8e1
Carlos <Carlos@CARLOS>
2020-01-31 21:07:59 +0100

    Mejora10

--- src/main/java/Decimales.java
+++ src/main/java/Decimales.java
@@ -1,6 +1,17 @@
 package main.java;
 
+import java.text.DecimalFormat;
+
 public class Decimales {
+	
+public static void sinDecimales (double decimal) {
+		
+		DecimalFormat formateado = new DecimalFormat("0");
+			
+		System.out.print("El numero redondeado sin decimales es: ");
+		System.out.println(formateado.format(Math.floor((decimal))));
+			
+	}
 
 	public static void main(String[] args) {
 		double decimal=5.7;
