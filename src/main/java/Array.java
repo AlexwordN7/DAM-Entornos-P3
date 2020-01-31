b5cc1c7ad82f1a025a8e5304648948d1b78edb1b
Carlos <Carlos@CARLOS>
2020-01-31 20:51:54 +0100

    Mejora2

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -1,7 +1,20 @@
 package main.java;
 
 public class Array {
-
+	
+	public static void NumeroMinimo (int[] numeros) {
+		int numeroMin=999;
+		
+		for(int i=0; i<numeros.length; i++) {
+				
+			if(numeros[i] < numeroMin){
+				numeroMin=numeros[i];
+			}
+			
+		}
+		System.out.println("El numero minimo es: "+numeroMin);
+	}
+	
 	public static void main(String[] args) {
 		int [] numeros={1,2,3,4,5};
 	}
--- src/main/java/Decimales.java
+++ src/main/java/Decimales.java
@@ -1,9 +1,28 @@
 package main.java;
 
+import java.text.DecimalFormat;
+
 public class Decimales {
 
+	public static void Redondear (double decimal) {
+			
+		System.out.print("El numero redondeado al alza es: ");
+		System.out.println(Math.round(decimal));
+			
+	}
+	
+	public static void eliminarDecimal (double decimal) {
+		
+		DecimalFormat formateado = new DecimalFormat("0");
+			
+		System.out.print("El numero redondeado sin decimales es: ");
+		System.out.println(formateado.format(Math.round(decimal)));
+			
+	}
 	public static void main(String[] args) {
 		double decimal=5.7;
+		Redondear(decimal);
+		eliminarDecimal(decimal);
 	}
 
 }
