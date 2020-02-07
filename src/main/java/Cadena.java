848474b9b8512801c63c368434e11b2a33047038
DAM_1 <DAM_1@PB01>
2020-02-07 18:56:30 +0100

    Mejora 7

--- src/main/java/Cadena.java
+++ src/main/java/Cadena.java
@@ -1,7 +1,17 @@
 package main.java;
 
 public class Cadena {
+	
+	public static void longitudNombre(String palabra) {
+		
+		System.out.println("La longitud del nombre es: " + palabra.length());
+		
+	}
+	
 	public static void main(String[] args) {
 		String hola="";
+		
+		longitudNombre(hola);
+
 	}
 }
