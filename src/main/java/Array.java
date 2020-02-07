8a4f419788cbba44755a6dbc7ab7b4d54b95da6a
DAM_1 <DAM_1@PB01>
2020-02-07 18:47:51 +0100

    Mejora 1

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -1,9 +1,31 @@
 package main.java;
 
 public class Array {
+	
+	
+	public static void numeroMaximo(int[]numeros) {
+		
+		int numeroMax=0;
+		
+		for(int i = 0; i<numeros.length;i++) {
+			
+			if(numeroMax < numeros[i]) {
+				
+				numeroMax = numeros[i];
+			}
+			
+			
+		}
+		
+		System.out.println("El numero maximo es: " + numeroMax);
+		
+		
+	}
 
 	public static void main(String[] args) {
 		int [] numeros={1,2,3,4,5};
+		
+		numeroMaximo(numeros);
 	}
 
 }
